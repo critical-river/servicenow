6a98a45eef2a1ad735ba3007abdb89a241873baa
Ramanjaneya633i <ramanjidaram2002@gmail.com>
2026-05-11 11:52:35 +0530

    testinging

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,9 @@
-
+<<<<<<< HEAD
+Hi i am learning the shell scripting for the firs time
+=======
 
 hi i am monitoring the shell script in java language
-its very diffult to make chnages
+its very diffult to make changes in either in python or java
 hi i am monitoring the shell script in python language
 
+>>>>>>> b7c80df (updated to shell)
