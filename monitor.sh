38ac05251bf2b54f1e06c5d81b0479153876c76a
Ramanjaneya633 <ramanjidaram2002@gmail.com>
2026-05-11 11:47:50 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-hi i am monitoring the shell script in java language
-its very diffult to make chnages
-=======
-hi i am monitoring the shell script in python language
->>>>>>> aea02d289136f4dbcfd78937ac829ac7b6c72216
+Hi i am learning the shell scripting for the firs time
